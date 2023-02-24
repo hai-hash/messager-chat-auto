459a59be77c401ea8b6b31f2841040bd5d76ee20
whynotme1131999 <whynotme1131999@gmail.com>
2023-02-24 15:31:54 +0700

    update code

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -31,6 +31,7 @@ let getWebhook = (req, res) => {
     let mode = req.query["hub.mode"];
     let token = req.query["hub.verify_token"];
     let challenge = req.query["hub.challenge"];
+    let a = "ZDZD"
 
     // Check if a token and mode is in the query string of the request
     if (mode && token) {
