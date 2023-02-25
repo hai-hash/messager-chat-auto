efc7c05805b6aadf3b3cddf071c675ca03e50662
whynotme1131999 <whynotme1131999@gmail.com>
2023-02-25 18:20:30 +0700

    update code

--- src/service/QuickReply.js
+++ src/service/QuickReply.js
@@ -1,4 +1,4 @@
-export function handleQuickReply(message){
+export function handleQuickReply(receivedMessage){
     let response;
     const payload = receivedMessage.quick_reply.payload;
     switch (payload) {
